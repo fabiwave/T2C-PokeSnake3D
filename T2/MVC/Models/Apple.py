673fad5ab3c4e665d0cd20722aecfea6424a332c
Fabiola <fabiolarivera@ug.uchile.cl>
2020-12-04 12:26:06 -0300

    Added light to the model apple

--- T2/MVC/Models/Apple.py
+++ T2/MVC/Models/Apple.py
@@ -48,7 +48,8 @@ class Apple(object):
         self.model = apple
         self.pos_x = self.t_delta + (random_x * self.grid_unit)
         self.pos_y = self.t_delta + (random_y * self.grid_unit)
-
+        self.eaten = 0
+        self.light_z = 10
         # Translation of the Apple to the random position
         self.model.transform = tr.translate(self.t_delta + (random_x * self.grid_unit),
                                             self.t_delta + (random_y * self.grid_unit), 0)
@@ -56,10 +57,40 @@ class Apple(object):
     # Draws the apple node into the scene
     def draw(self, pipeline_light, projection, view):
 
+        # light settings
+        self.light_setting(pipeline_light, view)
+        # gl program to draw the carrot itself
         glUseProgram(pipeline_light.shaderProgram)
         glUniformMatrix4fv(glGetUniformLocation(pipeline_light.shaderProgram, "projection"), 1, GL_TRUE, projection)
         glUniformMatrix4fv(glGetUniformLocation(pipeline_light.shaderProgram, "view"), 1, GL_TRUE, view)
         sg.drawSceneGraphNode(sg.findNode(self.model, 'carrot'), pipeline_light)
+
+    def light_setting(self, pipeline_light, view):
+        # Shininess of the object and z-location of the light depending on the amount of respawns
+        shiny = 100
+        z = 10
+        if self.eaten % 4 == 0:
+            shiny = 1
+            z = 1
+
+        camera_view = view[0]
+        glUseProgram(pipeline_light.shaderProgram)
+        glUniform3f(glGetUniformLocation(pipeline_light.shaderProgram, "La"), 1.0, 1.0, 1.0)
+        glUniform3f(glGetUniformLocation(pipeline_light.shaderProgram, "Ld"), 1.0, 1.0, 1.0)
+        glUniform3f(glGetUniformLocation(pipeline_light.shaderProgram, "Ls"), 1.0, 1.0, 1.0)
+
+        glUniform3f(glGetUniformLocation(pipeline_light.shaderProgram, "Ka"), 0.2, 0.2, 0.2)
+        glUniform3f(glGetUniformLocation(pipeline_light.shaderProgram, "Kd"), 0.9, 0.9, 0.9)
+        glUniform3f(glGetUniformLocation(pipeline_light.shaderProgram, "Ks"), 1.0, 1.0, 1.0)
+
+        glUniform3f(glGetUniformLocation(pipeline_light.shaderProgram, "lightPosition"), 0, 0, z)
+        glUniform3f(glGetUniformLocation(pipeline_light.shaderProgram, "viewPosition"), camera_view[0],
+                    camera_view[1], camera_view[2], )
+        glUniform1ui(glGetUniformLocation(pipeline_light.shaderProgram, "shininess"), shiny)
+        glUniform1f(glGetUniformLocation(pipeline_light.shaderProgram, "constantAttenuation"), 0.001)
+        glUniform1f(glGetUniformLocation(pipeline_light.shaderProgram, "linearAttenuation"), 0.1)
+        glUniform1f(glGetUniformLocation(pipeline_light.shaderProgram, "quadraticAttenuation"), 0.01)
+        glUniform1f(glGetUniformLocation(pipeline_light.shaderProgram, "quadraticAttenuation"), 0.01)
 
     # Returns the position of the apple
     def get_position(self):
@@ -71,7 +102,6 @@ class Apple(object):
 
     # Changes the position of the apple in the grid
     def respawn(self, snake):
-
         half_grid = int((self.total_grid - 2) / 2)
         if self.total_grid % 2 == 0:
             half_grid = half_grid - 1
@@ -91,3 +121,5 @@ class Apple(object):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.update_pos()
+        self.eaten += 1
+
